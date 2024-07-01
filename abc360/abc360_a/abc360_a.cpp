d33d6d084a6c98d27c04051f4fcce85968a0e161
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-07-01 01:35:11 +0000

    [abc360] abc360_a

--- abc360/abc360_a/abc360_a.cpp
+++ abc360/abc360_a/abc360_a.cpp
@@ -1,6 +1,5 @@
 #include <algorithm>
 #include <iostream>
-#include <set>
 #include <vector>
 #define ll long long
 #define fastio cin.tie(0)->sync_with_stdio(0);
@@ -8,14 +7,9 @@ using namespace std;
 
 int main() {
   fastio;
-  int r = 0, m = 0;
   string s;
   cin >> s;
-  for (int i = 0; i < 3; ++i) {
-    char c = s[i];
-    if (c == 'R') r = i;
-    else if (c == 'M') m = i;
-  }
-  if (r < m) cout << "Yes";
-  else cout << "No";
+  int a = s.find('R');
+  int b = s.find('M');
+  cout << ((a < b) ? "Yes" : "No");
 }
