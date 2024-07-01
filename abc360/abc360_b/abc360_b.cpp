f70cef18ba57a3987fe5e093ef81793840e4a7af
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-07-01 04:55:06 +0000

    [abc360] abc360_b

--- abc360/abc360_b/abc360_b.cpp
+++ abc360/abc360_b/abc360_b.cpp
@@ -1,42 +1,22 @@
 #include <algorithm>
 #include <iostream>
-#include <set>
 #include <vector>
 #define ll long long
 #define fastio cin.tie(0)->sync_with_stdio(0);
+#define si(x) int(x.size())
 using namespace std;
 
-vector<string> get_sub(string& s, int w) {
-  vector<string> sub;
-  string tmp = "";
-  int cnt = 0;
-  for (auto c : s) {
-    tmp += c;
-    if (++cnt == w) {
-      sub.push_back(tmp);
-      tmp = "";
-      cnt = 0;
-    }
-  }
-  sub.push_back(tmp);
-  return sub;
-}
 int main() {
   fastio;
-  string s, t;
+    string s, t, tmp;
   cin >> s >> t;
-  int n = s.size();
-  int k = t.size();
-  for (int w = 1; w < n; ++w) {
-    vector<string> sub = get_sub(s, w);
-    if ((int)sub.size() < k) break;
-    for (int c = 0; c < w; ++c) {
-      string s1 = "";
-      for (auto su : sub) {
-        if (su.size() <= c) break;
-        s1 += su[c];
+  for (int c = 1; c < si(s); ++c) {
+    for (int w = c; w < si(s); ++w) {
+      tmp = "";
+      for (int i = 0; i < si(s); i += w) {
+        if (i + c - 1 < si(s)) tmp += s[i + c - 1];
       }
-      if (!t.compare(s1)) {
+      if (tmp == t) {
         cout << "Yes";
         return 0;
       }
