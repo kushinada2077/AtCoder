091115be2826848aa3200bd8721a239609d1160f
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-07-06 12:06:01 +0000

    [abc361] abc361_a

---
+++ abc361/abc361_a/abc361_a.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+#include <stack>
+#include <vector>
+#define ll long long
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define FOR_IN_1(n) for (int i = 0; i < n; ++i)
+#define FOR_IN_2(i, n) for (int i = 0; i < n; ++i)
+#define FOR_IN_3(i, m, n) for (int i = m; i < n; ++i)
+#define GET_MACRO(_1, _2, _3, NAME, ...) NAME
+#define for_in(...) GET_MACRO(__VA_ARGS__, FOR_IN_3, FOR_IN_2, FOR_IN_1)(__VA_ARGS__)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(x) push_back(x)
+using namespace std;
+
+int main() {
+  fastio;
+    int n, k, x;
+  cin >> n >> k >> x;
+  vector<int> a(n);
+  for_in(n) cin >> a[i];
+  for_in(n) {
+    cout << a[i] << " ";
+    if (i + 1 == k) cout << x << " ";
+  }
+}
