903bd32d81a90cf68ade58173f672846fec84d54
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-07-20 12:02:58 +0000

    [abc363] abc363_a

---
+++ abc363/abc363_a/abc363_a.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
+#define ll long long
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+using namespace std;
+
+int main() {
+  fastio;
+    int r;
+  cin >> r;
+  if (r > 199) cout << 300 - r;
+  else if (r > 99) cout << 200 - r;
+  else cout << 100 - r;
+}
