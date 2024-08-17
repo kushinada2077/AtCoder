b88e98d6c227d5c1c44a4c37592d85955b58900d
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-08-17 12:04:47 +0000

    [abc367] abc367_a

---
+++ abc367/abc367_a/abc367_a.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <climits>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <vector>
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+using ll = long long;
+using namespace std;
+
+int main() {
+  fastio;
+    int a, b, c;
+  cin >> a >> b >> c;
+  if (b > c) {
+    a += 24;
+    c += 24;
+  }
+  if (b <= a && a <= c) cout << "No\n";
+  else cout << "Yes\n";
+}
