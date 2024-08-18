d20a0fc379c42f269464ba1d01c69cbd65ab8421
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-08-18 05:19:35 +0000

    [abc367] abc367_d

---
+++ abc367/abc367_d/abc367_d.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <climits>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <tuple>
+#include <vector>
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+using ll = long long;
+using namespace std;
+
+int n, m;
+int main() {
+  fastio;
+    cin >> n >> m;
+  vector<int> a(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+    a.pb(a[i]);
+  }
+
+  vector<int> r;
+  for (int i = 0; i < 2 * n; ++i) r.pb(((si(r) ? r.back() : 0) + a[i]) % m);
+
+  vector<int> b(m, 0);
+  ll ans = 0;
+  for (int i = 1; i < n; ++i) b[r[i]]++;
+  for (int i = n; i < 2 * n; ++i) {
+    ans += b[r[i]];
+    b[r[i]]++;
+    b[r[i - n + 1]]--;
+  }
+  cout << ans << "\n";
+}
