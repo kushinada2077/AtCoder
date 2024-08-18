e211898b79a29cddff409fbc4602484447243345
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-08-18 10:35:34 +0000

    [abc167] abc167_d

---
+++ abc167/abc167_d/abc167_d.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <climits>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <tuple>
+#include <vector>
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+using ll = long long;
+using namespace std;
+
+const int MX_D = 60;
+int nxt[200005][MX_D];
+int main() {
+  fastio;
+    int n;
+  ll k;
+  cin >> n >> k;
+  for (int i = 1; i <= n; ++i) cin >> nxt[i][0];
+  for (int j = 1; j < MX_D; ++j) {
+    for (int i = 1; i <= n; ++i) nxt[i][j] = nxt[nxt[i][j - 1]][j - 1];
+  }
+  int ans = 1;
+  for (int j = 0; j < MX_D; ++j) {
+    if (k & (1ll << j)) {
+      ans = nxt[ans][j];
+    }
+  }
+  cout << ans << "\n";
+}
