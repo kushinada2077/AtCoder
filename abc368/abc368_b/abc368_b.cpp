86f0af10f5a9c5ce0d3bb5c5485bfa533d3ce759
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-08-24 12:08:03 +0000

    [abc368] abc368_b

---
+++ abc368/abc368_b/abc368_b.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <climits>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <vector>
+#define PATH "/Users/leedongha/Downloads/PS/input.txt"
+#define fastio cin.tie(0)->sync_with_stdio(0);
+#define for_in(n) for (int i = 0; i < n; ++i)
+#define si(x) int(x.size())
+#define all(x) (x).begin(), (x).end()
+#define pb(...) push_back(__VA_ARGS__)
+#define X first
+#define Y second
+#define ROOT 1
+using ll = long long;
+using namespace std;
+
+int main() {
+  fastio;
+    int n;
+  cin >> n;
+  vector<int> a(n);
+  for (auto& i : a) cin >> i;
+  int ans = 0;
+  while (true) {
+    int cnt = 0;
+    for (int i = 0; i < n; ++i)
+      if (a[i] > 0) cnt++;
+    if (cnt <= 1) {
+      cout << ans << "\n";
+      return 0;
+    }
+    sort(all(a), greater<>());
+    a[0]--;
+    a[1]--;
+    ans++;
+  }
+}
