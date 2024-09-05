48fbe8034a7db70d1777ea443d8dc8857134b4d0
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-09-05 05:34:27 +0000

    [abc338] abc338_d

--- abc338/abc338_d/abc338_d.cpp
+++ abc338/abc338_d/abc338_d.cpp
@@ -29,34 +29,24 @@ int main() {
     int n, m;
   cin >> n >> m;
   vector<int> x(m);
-  for (int &i : x) {
-    cin >> i;
-    --i;
+  vector<ll> d(n + 5);
+  for (auto& i : x) cin >> i;
+
+  for (int i = 0; i < m - 1; ++i) {
+    ll from = min(x[i], x[i + 1]), to = max(x[i], x[i + 1]);
+    ll d1 = to - from, d2 = from + n - to;
+    d[from] += d2;
+    d[to] -= d2;
+    d[0] += d1;
+    d[from] -= d1;
+    d[to] += d1;
+    d[n] -= d1;
   }
-  vector<ll> v(n + 1);
-  auto dist = [&](int from, int to) {
-    if (from <= to) return to - from;
-    else return to + n - from;
-  };
-  auto add = [&](int from, int to, int num) {
-    if (from <= to) {
-      v[from] += num;
-      v[to] -= num;
-    } else {
-      v[from] += num;
-      v[n] -= num;
-      v[0] += num;
-      v[to] -= num;
-    }
-  };
-  for (int i = 0; i < m - 1; i++) {
-    add(x[i], x[i + 1], dist(x[i + 1], x[i]));
-    add(x[i + 1], x[i], dist(x[i], x[i + 1]));
-  }
-  ll ans = 1LL << 60;
-  for (int i = 0; i < n; i++) {
-    v[i + 1] += v[i];
-    ans = min(ans, v[i]);
+
+  ll ans = LLONG_MAX, tot = 0;
+  for (int i = 0; i < n; ++i) {
+    tot += 1ll * d[i];
+    ans = min(ans, tot);
   }
   cout << ans << "\n";
 }
