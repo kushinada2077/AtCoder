e4baec2e459eeca5270908bc1f13f3f5b792d6dc
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-12-24 14:27:08 +0000

    [abc256] abc256_c

--- abc256/abc256_c/abc256_c.cpp
+++ abc256/abc256_c/abc256_c.cpp
@@ -21,36 +21,40 @@ using ll = long long;
 using TP = tuple<int, int, int>;
 using P = pair<int, int>;
 
-int h[3], w[3], board[3][3], ans;
+int h[3], w[3], a[3][3], ans;
+void solve(int ij) {
+  int i = ij / 3, j = ij % 3;
+  if (i == 3) {
+    ans++;
+    return;
+  }
+
+  if (i == 2) {
+    int x = w[j] - a[1][j] - a[0][j];
+    if (x < 1) return;
+    a[i][j] = x;
+    solve(ij + 1);
+  } else if (j == 2) {
+    int x = h[i] - a[i][1] - a[i][0];
+    if (x < 1) return;
+    a[i][j] = x;
+    solve(ij + 1);
+  } else {
+    for (int x = 1; x < min(h[i], w[j]); ++x) {
+      a[i][j] = x;
+      solve(ij + 1);
+    }
+  }
+}
 int main() {
   fastio;
     for (int i = 0; i < 3; ++i) cin >> h[i];
   for (int i = 0; i < 3; ++i) cin >> w[i];
-
-  for (int ul = 1; ul < h[0] - 1; ++ul) {
-    for (int ur = 1; ul + ur < h[0]; ++ur) {
-      for (int dl = 1; dl < h[1] - 1; ++dl) {
-        for (int dr = 1; dl + dr < h[1]; ++dr) {
-          bool fail = false;
-          board[0][0] = ul;
-          board[0][1] = ur;
-          board[1][0] = dl;
-          board[1][1] = dr;
-          board[0][2] = h[0] - ul - ur;
-          board[1][2] = h[1] - dl - dr;
-          for (int i = 0; i < 3; ++i) {
-            if ((board[2][i] = w[i] - board[1][i] - board[0][i]) < 1) {
-              fail = true;
-              break;
-            }
-          }
-
-          if (board[2][0] + board[2][1] + board[2][2] != h[2]) fail = true;
-          if (!fail) ans++;
-        }
-      }
-    }
+  if (h[0] + h[1] + h[2] != w[0] + w[1] + w[2]) {
+    cout << "0\n";
+    return 0;
   }
+  solve(0);
 
   cout << ans << "\n";
 }
