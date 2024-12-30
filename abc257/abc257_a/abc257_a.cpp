665338e4079b9d77172dcd7b9d07e42661563e01
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-12-30 08:14:31 +0000

    [abc257] abc257_a

--- abc257/abc257_a/abc257_a.cpp
+++ abc257/abc257_a/abc257_a.cpp
@@ -4,10 +4,10 @@ using i64 = long long;
 using TPi = tuple<int, int, int>;
 using Pi = pair<int, int>;
 
+int n, x;
 int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
-  int n, x;
   cin >> n >> x;
-  char res = 'A' + ((x + n - 1) / n) - 1;
-  cout << res << "\n";
+  x--;
+  cout << (char)('A' + (x / n));
 }
