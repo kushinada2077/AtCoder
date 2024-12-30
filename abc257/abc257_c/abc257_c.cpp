a291210f5b7cbb725c09c1d7ba1474947cee6f0a
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-12-30 08:27:01 +0000

    [abc257] abc257_c

--- abc257/abc257_c/abc257_c.cpp
+++ abc257/abc257_c/abc257_c.cpp
@@ -4,27 +4,27 @@ using i64 = long long;
 using TPi = tuple<int, int, int>;
 using Pi = pair<int, int>;
 
-int n, w;
+int n, W[200005];
 string s;
-vector<int> adult, children, all;
+vector<int> child, adult, all;
 int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cin >> n >> s;
   for (int i = 0; i < n; ++i) {
-    cin >> w;
-    if (s[i] == '0') children.push_back(w);
-    else adult.push_back(w);
-    all.push_back(w);
+    cin >> W[i];
+    all.push_back(W[i]);
+    if (s[i] == '0') child.push_back(W[i]);
+    else adult.push_back(W[i]);
   }
-  sort(children.begin(), children.end());
+
+  sort(child.begin(), child.end());
   sort(adult.begin(), adult.end());
 
-  int ans = max(children.size(), adult.size());
-
-  for (auto x : all) {
-    int numChild = lower_bound(children.begin(), children.end(), x) - children.begin();
-    int numAdult = (int)adult.size() - (lower_bound(adult.begin(), adult.end(), x) - adult.begin());
-    ans = max(ans, numChild + numAdult);
+  int ans = max(child.size(), adult.size());
+  for (auto w : all) {
+    int cn = lower_bound(child.begin(), child.end(), w) - child.begin();
+    int an = adult.end() - lower_bound(adult.begin(), adult.end(), w);
+    ans = max(ans, an + cn);
   }
 
   cout << ans << "\n";
