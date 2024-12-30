9ae5171ebb647a6e7d7ab9cd292df306a5bba64a
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-12-30 08:38:26 +0000

    [abc257] abc257_c

--- abc257/abc257_c/abc257_c.cpp
+++ abc257/abc257_c/abc257_c.cpp
@@ -4,27 +4,22 @@ using i64 = long long;
 using TPi = tuple<int, int, int>;
 using Pi = pair<int, int>;
 
-int n, W[200005];
+int n, res, ans;
 string s;
-vector<int> child, adult, all;
+map<int, int> f;
 int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cin >> n >> s;
-  for (int i = 0; i < n; ++i) {
-    cin >> W[i];
-    all.push_back(W[i]);
-    if (s[i] == '0') child.push_back(W[i]);
-    else adult.push_back(W[i]);
+  for (int w, i = 0; i < n; ++i) {
+    cin >> w;
+    res += s[i] == '1';
+    f[w] += s[i] == '0' ? 1 : -1;
   }
 
-  sort(child.begin(), child.end());
-  sort(adult.begin(), adult.end());
-
-  int ans = max(child.size(), adult.size());
-  for (auto w : all) {
-    int cn = lower_bound(child.begin(), child.end(), w) - child.begin();
-    int an = adult.end() - lower_bound(adult.begin(), adult.end(), w);
-    ans = max(ans, an + cn);
+  ans = res;
+  for (auto [_, x] : f) {
+    res += x;
+    ans = max(ans, res);
   }
 
   cout << ans << "\n";
