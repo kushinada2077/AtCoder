f43bdb7203fba5f36ccdc9420a8977155bfc3bde
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-12-31 01:58:50 +0000

    [abc258] abc258_a

--- abc258/abc258_a/abc258_a.cpp
+++ abc258/abc258_a/abc258_a.cpp
@@ -1,17 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 using i64 = long long;
-using TPi = tuple<int, int, int>;
-using Pi = pair<int, int>;
 
 int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
-  const int M = 60 * 24;
   int k;
   cin >> k;
-  k += 60 * 21;
-  k %= M;
-  cout << k / 60 << ":";
-  if ((k % 60) < 10) cout << "0";
-  cout << k % 60 << "\n";
+  cout << 21 + k / 60 << ":" << (k % 60) / 10 << k % 60 % 10 << "\n";
 }
