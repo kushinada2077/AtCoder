f7067ec5d579e25b088cf2ed538b88336c5f049f
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-12-31 02:47:35 +0000

    [abc258] abc258_d

--- abc258/abc258_d/abc258_d.cpp
+++ abc258/abc258_d/abc258_d.cpp
@@ -1,22 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 using i64 = long long;
-using TPi = tuple<int, int, int>;
-using Pi = pair<int, int>;
 
-const int MX = 2e5 + 5;
 int n;
-i64 x, A[MX], B[MX];
+i64 x, A[200005], B[200005];
+
 int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cin >> n >> x;
   for (int i = 0; i < n; ++i) cin >> A[i] >> B[i];
-  i64 minGameT = B[0], baseT = A[0] + B[0], ans = A[0] + B[0] + (x - 1) * B[0];
-  int clearNum = 2;
-  for (int i = 1; i < n; ++i, clearNum++) {
-    minGameT = min(minGameT, B[i]);
-    baseT += A[i] + B[i];
-    if (x > clearNum) ans = min(ans, baseT + (x - clearNum) * minGameT);
+  i64 ans = A[0] + B[0] + (x - 1) * B[0], sum = A[0] + B[0], min_cost = B[0];
+
+  for (int i = 1; i < n; ++i) {
+    min_cost = min(min_cost, B[i]);
+    sum += A[i] + B[i];
+    ans = min(ans, sum + (x - i - 1) * min_cost);
   }
 
   cout << ans << "\n";
