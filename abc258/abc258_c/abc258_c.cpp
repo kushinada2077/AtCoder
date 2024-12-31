d07a81162930728b53d137d634b67997f144d740
kushinada <111179843+kushinada2077@users.noreply.github.com>
2024-12-31 02:20:01 +0000

    [abc258] abc258_c

--- abc258/abc258_c/abc258_c.cpp
+++ abc258/abc258_c/abc258_c.cpp
@@ -1,18 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 using i64 = long long;
-using TPi = tuple<int, int, int>;
-using Pi = pair<int, int>;
 
 int n, q;
 string s;
 int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cin >> n >> q >> s;
-  int idx = 0;
+  int prefix = 0;
   for (int a, x, i = 0; i < q; ++i) {
     cin >> a >> x;
-    if (a == 1) idx = (idx + (n - x)) % n;
-    else cout << s[(idx + x - 1) % n] << "\n";
+    if (a == 1) prefix = (prefix + n - x) % n;
+    if (a == 2) cout << s[(prefix + x - 1) % n] << "\n";
   }
 }
