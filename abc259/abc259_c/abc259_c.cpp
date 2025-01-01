cb6d681b7cae2d023ca338e02dd2679b9fc9934b
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-01-01 04:14:23 +0000

    [abc259] abc259_c

--- abc259/abc259_c/abc259_c.cpp
+++ abc259/abc259_c/abc259_c.cpp
@@ -3,29 +3,37 @@ using namespace std;
 using i64 = long long;
 
 string s, t;
-bool compare(const string& s, const string& t) {
-  int sl = s.size(), tl = t.size(), a, b;
-  a = b = 0;
-
-  while (a < sl && b < tl) {
-    if (s[a] != t[b]) return false;
-    if (0 < a && 0 < b && s[a - 1] == s[a]) {
-      while (a < sl && s[a - 1] == s[a] && s[a] == t[b]) {
-        a++;
-        b++;
-      }
-      while (b < tl && s[a - 1] == t[b]) b++;
-    } else {
-      a++;
-      b++;
+vector<pair<char, int>> divide(const string& s) {
+  vector<pair<char, int>> ret;
+  int l = s.size(), i = 0;
+  while (i < l) {
+    int cnt = 1;
+    while (i < l - 1 && s[i] == s[i + 1]) {
+      i++;
+      cnt++;
     }
+    ret.push_back({s[i], cnt});
+    i++;
   }
 
-  return a == sl && b == tl;
+  return ret;
 }
 int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cin >> s >> t;
-  bool ans = compare(s, t);
-  cout << (ans ? "Yes" : "No") << "\n";
+  auto sseg = divide(s), tseg = divide(t);
+  bool ok = true;
+  if (sseg.size() != tseg.size()) ok = false;
+  for (int i = 0; i < sseg.size(); ++i) {
+    auto [sc, sn] = sseg[i];
+    auto [tc, tn] = tseg[i];
+    if (sc != tc) ok = false;
+    else if (sn == 1 && tn > 1 || sn > tn) ok = false;
+    if (!ok) {
+      cout << "No\n";
+      return 0;
+    }
+  }
+
+  cout << "Yes\n";
 }
