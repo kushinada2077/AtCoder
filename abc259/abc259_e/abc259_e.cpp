3b407191e04a88d86b61047f9744a1f585548974
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-01-01 06:31:59 +0000

    [abc259] abc259_e

--- abc259/abc259_e/abc259_e.cpp
+++ abc259/abc259_e/abc259_e.cpp
@@ -3,42 +3,38 @@ using namespace std;
 using i64 = long long;
 
 int n;
-map<int, multiset<int>> pe;
-map<int, vector<pair<int, int>>> ipe;
-set<pair<int, int>> ans;
+map<int, array<int, 2>> maxe;
 int main() {
   cin.tie(nullptr)->sync_with_stdio(false);
   cin >> n;
+  vector<vector<array<int, 2>>> a(n);
   for (int m, i = 0; i < n; ++i) {
     cin >> m;
+    a[i].resize(m);
     for (int p, e, j = 0; j < m; ++j) {
       cin >> p >> e;
-      pe[p].insert(e);
-      ipe[i].push_back({p, e});
+      auto& [v, c] = maxe[p];
+      if (v < e) {
+        v = e;
+        c = 1;
+      } else if (v == e) c++;
+      a[i][j] = {p, e};
     }
   }
 
+  int ans = 0;
+  bool no = false;
   for (int i = 0; i < n; ++i) {
-    auto solve = [&](int i) -> void {
-      for (auto [p, e] : ipe[i]) {
-        if (pe[p].size() == 1) {
-          ans.insert({p, e});
-          return;
-        } else {
-          auto back = --pe[p].end();
-          auto back_pre = --(--pe[p].end());
-          if (*back == e && *back_pre != *back) {
-            ans.insert({p, e});
-            return;
-          }
-        }
-      }
+    bool ok = false;
+    for (auto [p, e] : a[i]) {
+      if (maxe[p] == array{e, 1}) ok = true;
+      if (ok) break;
+    }
 
-      ans.insert({0, 0});
-    };
-
-    solve(i);
+    ans += ok;
+    no |= !ok;
   }
 
-  cout << ans.size() << "\n";
+  ans += no;
+  cout << ans << "\n";
 }
