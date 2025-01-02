c16a1f17f3767cced304854be59d694e03d81bce
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-01-02 09:15:27 +0000

    [abc260] abc260_e

--- abc260/abc260_e/abc260_e.cpp
+++ abc260/abc260_e/abc260_e.cpp
@@ -1,34 +1,39 @@
 #include <bits/stdc++.h>
 using i64 = long long;
 
-int n, m;
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
+  int n, m, allcnt = 0;
   std::cin >> n >> m;
-  std::vector<int> f(m);
-  int maxl = m - 1, minr = 0;
+  std::vector<int> a(n), b(n), cnt(n), ans(m + 3);
+  std::vector<std::vector<int>> inv(m + 1);
   for (int i = 0; i < n; ++i) {
-    int a, b;
-    std::cin >> a >> b;
-    a--;
-    b--;
-    maxl = std::min(maxl, b);
-    minr = std::max(minr, a);
-    f[a] = std::max(f[a], b);
+    std::cin >> a[i] >> b[i];
+    inv[a[i]].push_back(i);
+    inv[b[i]].push_back(i);
   }
+  for (int i = 1, j = 1; i <= m;) {
+    while (j <= m && allcnt < n) {
+      for (auto v : inv[j]) {
+        if (cnt[v] == 0) allcnt++;
+        cnt[v]++;
+      }
+      j++;
+    }
 
-  std::vector<int> ans(m);
-  for (int i = 0; i <= maxl; ++i) {
-    ans[minr - i + 1]++;
-    ans[m - i + 1]--;
-    minr = std::max(minr, f[i]);
-  }
+    if (allcnt < n) break;
+    ans[j - i]++;
+    ans[m - i + 2]--;
 
-  for (int i = 2; i <= m; ++i) {
-    ans[i] += ans[i - 1];
+    for (auto v : inv[i]) {
+      cnt[v]--;
+      if (cnt[v] == 0) allcnt--;
+    }
+    i++;
   }
 
   for (int i = 1; i <= m; ++i) {
+    ans[i] += ans[i - 1];
     std::cout << ans[i] << " \n"[i == m];
   }
 }
