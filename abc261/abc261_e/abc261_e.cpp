c6418791ac137e346a024b28c4aec734d13ef89a
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-01-03 06:32:18 +0000

    [abc261] abc261_e

--- abc261/abc261_e/abc261_e.cpp
+++ abc261/abc261_e/abc261_e.cpp
@@ -14,17 +14,12 @@ int main() {
   std::vector<std::vector<std::array<int, 2>>> f(30, std::vector<std::array<int, 2>>(n));
   for (int i = 0; i < n; ++i) std::cin >> t[i] >> a[i];
   for (int k = 0; k < 30; ++k) {
-    int b[2] = {0, 1};
-    for (int i = 0; i < n; ++i) {
-      b[0] = f[k][i][0] = operate(b[0], (a[i] >> k) & 1, t[i]);
-      b[1] = f[k][i][1] = operate(b[1], (a[i] >> k) & 1, t[i]);
-    }
-  }
-
-  for (int k = 0; k < 30; ++k) {
     int bit = (c >> k) & 1;
+    int arr[2] = {0, 1};
     for (int i = 0; i < n; ++i) {
-      bit = f[k][i][bit];
+      arr[0] = operate(arr[0], (a[i] >> k) & 1, t[i]);
+      arr[1] = operate(arr[1], (a[i] >> k) & 1, t[i]);
+      bit = arr[bit];
       ans[i] |= (bit << k);
     }
   }
