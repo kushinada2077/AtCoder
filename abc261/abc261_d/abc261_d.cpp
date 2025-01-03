9bc8f9d501578a15b72b15a3f3bc100fcc735878
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-01-03 07:37:29 +0000

    [abc261] abc261_d

--- abc261/abc261_d/abc261_d.cpp
+++ abc261/abc261_d/abc261_d.cpp
@@ -1,28 +1,27 @@
 #include <bits/stdc++.h>
 using i64 = long long;
 
+constexpr i64 inf = 1e18;
+
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
   int n, m;
   std::cin >> n >> m;
-  std::vector<i64> x(n), c(n + 1);
-  std::vector<std::vector<i64>> dp(n, std::vector<i64>(n + 1, 0));
+  std::vector<i64> x(n), c(n + 1), dp(n + 1);
   for (int i = 0; i < n; ++i) std::cin >> x[i];
   for (int i = 0; i < m; ++i) {
     int a;
-    i64 b;
-    std::cin >> a >> b;
-    c[a] = b;
+    std::cin >> a >> c[a];
   }
 
-  dp[0][1] = x[0] + c[1];
-
-  for (int i = 1; i < n; ++i) {
-    dp[i][0] = *max_element(dp[i - 1].begin(), dp[i - 1].end());
-    for (int j = 1; j <= i + 1; ++j) {
-      dp[i][j] = dp[i - 1][j - 1] + x[i] + c[j];
+  for (int i = 0; i < n; ++i) {
+    std::vector<i64> g(n + 1);
+    for (int j = 0; j <= i; ++j) {
+      g[j + 1] = std::max(g[j + 1], dp[j] + x[i] + c[j + 1]);
+      g[0] = std::max(g[0], dp[j]);
     }
+    dp = g;
   }
 
-  std::cout << *max_element(dp[n - 1].begin(), dp[n - 1].end()) << "\n";
+  std::cout << *max_element(dp.begin(), dp.end()) << "\n";
 }
