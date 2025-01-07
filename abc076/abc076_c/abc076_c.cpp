e258006de50e41e410ace28d7514725fc1d83a21
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-01-07 11:48:40 +0000

    [abc076] abc076_c

--- abc076/abc076_c/abc076_c.cpp
+++ abc076/abc076_c/abc076_c.cpp
@@ -21,8 +21,8 @@ int main() {
       for (j = i; j < i + m; ++j) {
         s[j] = t[j - i];
       }
-      for (int i = 0; i < n; ++i) {
-        if (s[i] == '?') s[i] = 'a';
+      for (int k = 0; k < n; ++k) {
+        if (s[k] == '?') s[k] = 'a';
       }
 
       std::cout << s << "\n";
