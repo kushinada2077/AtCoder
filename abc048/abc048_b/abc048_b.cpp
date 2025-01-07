594cfc46ab5e1daaf6a9767d044f1c8da5bdffea
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-01-07 11:20:15 +0000

    [abc048] abc048_b

--- abc048/abc048_b/abc048_b.cpp
+++ abc048/abc048_b/abc048_b.cpp
@@ -3,10 +3,9 @@ using i64 = long long;
 
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
-  i64 a, b, x, ans = 0;
+  i64 a, b, x;
   std::cin >> a >> b >> x;
-  if (a % x == 0) ans++;
-  a -= (a % x);
-  ans += (b - a) / x;
+  i64 ans = (b / x) - (std::max(0LL, a - 1) / x);
+  if (a == 0) ans++;
   std::cout << ans << "\n";
 }
