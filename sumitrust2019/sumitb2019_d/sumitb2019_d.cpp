a826e219da5f67097389c60f3c9c93743f0ce343
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-01-08 12:27:38 +0000

    [sumitrust2019] sumitb2019_d

---
+++ sumitrust2019/sumitb2019_d/sumitb2019_d.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+
+bool ok(const std::string& s, const std::string& t) {
+  int n = s.size(), m = t.size();
+  for (int i = 0, j = 0; i < n; ++i) {
+    while (j < m && s[i] != t[j]) {
+      j++;
+    }
+    if (j == m) return false;
+    j++;
+  }
+
+  return true;
+}
+int main() {
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  int n;
+  i64 ans = 0;
+  std::string s;
+  std::cin >> n >> s;
+  for (int i = 0; i < 1000; ++i) {
+    std::string a = "";
+    a += i / 100 + '0';
+    a += i / 10 % 10 + '0';
+    a += i % 10 + '0';
+    ans += ok(a, s);
+  }
+
+  std::cout << ans << "\n";
+}
