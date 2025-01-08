960baa6a84821276f8dc349be0bae20fa1157e71
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-01-08 13:48:07 +0000

    [abc125] abc125_d

--- abc125/abc125_d/abc125_d.cpp
+++ abc125/abc125_d/abc125_d.cpp
@@ -2,19 +2,21 @@
 using i64 = long long;
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
-  int n;
+  int n, s = 0, z = 0;
+  i64 ans = 0;
   std::cin >> n;
   std::vector<i64> a(n);
-  // dp[i][j] = i번째까지일 때 최대 합 i번째는 누름(j == 1), 안 누름(j == 0)
-  std::vector dp(n + 1, std::vector<i64>(2, 0));
-  for (int i = 0; i < n; ++i) std::cin >> a[i];
-  dp[0][0] = a[0];
-  dp[0][1] = -a[0];
-
-  for (int i = 0; i < n - 1; ++i) {
-    dp[i + 1][0] = std::max(dp[i][0] + a[i + 1], dp[i][1] - a[i + 1]);
-    dp[i + 1][1] = std::max(dp[i][0] - a[i + 1], dp[i][1] + a[i + 1]);
+  for (int i = 0; i < n; ++i) {
+    std::cin >> a[i];
+    ans += std::abs(a[i]);
+    z += a[i] == 0;
+    s += a[i] < 0;
+    if (a[i] < 0) a[i] *= -1;
   }
 
-  std::cout << dp[n - 1][0] << "\n";
+  if (!z && s % 2) {
+    ans -= 2 * *min_element(a.begin(), a.end());
+  }
+
+  std::cout << ans << "\n";
 }
