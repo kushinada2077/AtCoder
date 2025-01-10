ce331b7d895be55fa121f9e84a50068b3cb6bd1f
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-01-10 09:02:19 +0000

    [code-festival-2016-qualc] codefestival_2016_qualC_b

--- code-festival-2016-qualc/codefestival_2016_qualC_b/codefestival_2016_qualC_b.cpp
+++ code-festival-2016-qualc/codefestival_2016_qualC_b/codefestival_2016_qualC_b.cpp
@@ -3,36 +3,13 @@ using i64 = long long;
 
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
-  int n, t, ans = 0;
-  std::cin >> n >> t;
+  int k, t, ans = 0;
+  std::cin >> k >> t;
   std::vector<int> a(t);
-  std::priority_queue<std::pair<int, int>> pq;
   for (int i = 0; i < t; ++i) {
     std::cin >> a[i];
-    pq.push({a[i], i});
-  }
-  int pre = -1;
-
-  while (!pq.empty()) {
-    auto [num, v] = pq.top();
-    pq.pop();
-
-    if (pre == v) {
-      if (!pq.empty()) {
-        auto [nnum, nv] = pq.top();
-        pq.pop();
-        pq.push({num, v});
-        if (nnum > 1) pq.push({nnum - 1, nv});
-        pre = nv;
-      } else {
-        ans++;
-        if (num > 1) pq.push({num - 1, v});
-      }
-    } else {
-      if (num > 1) pq.push({num - 1, v});
-      pre = v;
-    }
   }
 
-  std::cout << ans << "\n";
+  int max = *max_element(a.begin(), a.end());
+  std::cout << std::max(max - 1 - (k - max), 0);
 }
