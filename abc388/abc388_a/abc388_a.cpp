4f78eae79fe4ca05c518f34aa9bbe9f795c4e895
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-01-11 12:02:51 +0000

    [abc388] abc388_a

---
+++ abc388/abc388_a/abc388_a.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+
+int main() {
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  std::string s;
+  std::cin >> s;
+  std::cout << std::string(1, s[0]) + "UPC\n";
+}
