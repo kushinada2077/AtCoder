d3f79c194e791d27cc97cacfcf421f1a7de9c6a0
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-01-11 02:35:23 +0000

    [abc138] abc138_d

--- abc138/abc138_d/abc138_d.cpp
+++ abc138/abc138_d/abc138_d.cpp
@@ -5,7 +5,8 @@ int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
   int n, q;
   std::cin >> n >> q;
-  std::vector<int> adj[n + 1], c(n + 1), ans(n + 1);
+  std::vector<int> adj[n + 1], c(n + 1), s(n + 1), e(n + 1), inv(n + 1);
+  std::vector<bool> vis(n + 1);
   for (int i = 0; i < n - 1; ++i) {
     int u, v;
     std::cin >> u >> v;
@@ -13,24 +14,37 @@ int main() {
     adj[v].push_back(u);
   }
 
+  int sn = 1;
+  std::function<void(int)> dfs = [&](int u) {
+    vis[u] = true;
+    inv[sn] = u;
+    s[u] = sn++;
+    for (auto v : adj[u]) {
+      if (!vis[v]) {
+        dfs(v);
+      }
+    }
+    e[u] = sn;
+  };
+
+  dfs(1);
+
   for (int i = 0; i < q; ++i) {
     int p, x;
     std::cin >> p >> x;
-    c[p] += x;
+    c[s[p]] += x;
+    if (e[p] <= n) c[e[p]] -= x;
   }
 
-  std::vector<bool> vis(n + 1);
-  std::function<void(int, int)> dfs = [&](int u, int cnt) {
-    vis[u] = true;
-    ans[u] = cnt + c[u];
-    for (auto v : adj[u]) {
-      if (!vis[v]) {
-        dfs(v, ans[u]);
-      }
-    }
-  };
+  for (int i = 1; i < n; ++i) {
+    c[i + 1] += c[i];
+  }
 
-  dfs(1, 0);
+  std::vector<int> ans(n + 1);
+  for (int i = 1; i <= n; ++i) {
+    ans[inv[i]] = c[i];
+  }
+
   for (int i = 1; i <= n; ++i) {
     std::cout << ans[i] << " \n"[i == n];
   }
