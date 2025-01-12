ccb3b864fd165da8a719454c11e534dcb92085d7
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-01-12 07:59:22 +0000

    [abc388] abc388_d

--- abc388/abc388_d/abc388_d.cpp
+++ abc388/abc388_d/abc388_d.cpp
@@ -5,23 +5,20 @@ int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
   int n;
   std::cin >> n;
-  std::vector<i64> a(n), dt(n + 2), ans(n + 2);
+  std::vector<int> a(n), d(n + 1);
   for (int i = 0; i < n; ++i) {
     std::cin >> a[i];
   }
 
-  i64 d = 0;
+  int cnt = 0;
   for (int i = 0; i < n; ++i) {
-    d += dt[i];
-    i64 cur = a[i] + d;
-    ans[i] = cur;
-    if (cur > 0) {
-      d++;
-      if (i + cur + 1 < n + 2) dt[i + cur + 1]--;
-    }
+    cnt += d[i];
+    a[i] += cnt;
+    cnt++;
+    d[std::min(n, i + a[i] + 1)]--;
   }
 
   for (int i = 0; i < n; ++i) {
-    std::cout << std::max(0LL, ans[i] - (n - i - 1)) << " \n"[i == n - 1];
+    std::cout << std::max(0, a[i] - n + i + 1) << " ";
   }
 }
