3d63f3b8226a753ccc04f082c5cba64d849b22c0
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-01-16 13:44:17 +0000

    [ddcc2020-qual] ddcc2020_qual_c

--- ddcc2020-qual/ddcc2020_qual_c/ddcc2020_qual_c.cpp
+++ ddcc2020-qual/ddcc2020_qual_c/ddcc2020_qual_c.cpp
@@ -7,50 +7,70 @@ int main() {
   std::cin >> h >> w >> k;
   std::vector b(h, std::vector<char>(w));
   std::vector ans(h, std::vector<int>(w, 0));
-  std::vector<int> p;
-  p.push_back(0);
-  bool m = false;
   for (int i = 0; i < h; ++i) {
-    bool s = false;
     for (int j = 0; j < w; ++j) {
       std::cin >> b[i][j];
-      if (b[i][j] == '#') {
-        s = true;
-      }
-    }
-    if (s == true && m == false) {
-      m = true;
-    } else if (s == true && m == true) {
-      p.push_back(i);
     }
   }
 
-  p.push_back(h);
+  int n = 1;
+  std::function<void(int, int, int, int)> solve = [&](int sy, int sx, int ey, int ex) {
+    int c = 0;
+    for (int y = sy; y < ey; ++y) {
+      for (int x = sx; x < ex; ++x) {
+        c += b[y][x] == '#';
+      }
+    }
 
-  int n = p.size(), c = 1;
-  for (int i = 0; i < n - 1; ++i) {
-    bool m = false;
-    int ni = p[i + 1];
-    for (int x = 0; x < w; ++x) {
-      bool s = false;
-      for (int y = p[i]; y < ni; ++y) {
-        if (b[y][x] == '#') {
-          s = true;
+    if (c == 1) {
+      for (int y = sy; y < ey; ++y) {
+        for (int x = sx; x < ex; ++x) {
+          ans[y][x] = n;
         }
       }
-      if (m == false && s == true) {
-        m = true;
-      } else if (m == true && s == true) {
-        c++;
-      }
-
-      for (int y = p[i]; y < ni; ++y) {
-        ans[y][x] = c;
-      }
+      n++;
     }
-    c++;
-  }
 
+    else {
+      bool r = false;
+      int ny = -1, nx = -1;
+      for (int y = sy; y < ey; ++y) {
+        int c = 0;
+        for (int x = sx; x < ex; ++x) {
+          if (b[y][x] == '#') {
+            c++;
+            if (ny == -1) {
+              ny = y;
+            }
+          }
+        }
+        if (c > 1) {
+          r = true;
+        }
+      }
+
+      for (int x = sx; x < ex; ++x) {
+        for (int y = sy; y < ey; ++y) {
+          if (b[y][x] == '#' && nx == -1) {
+            nx = x;
+            break;
+          }
+        }
+      }
+
+      int mid;
+      if (r == true) {
+        solve(sy, sx, ey, nx + 1);
+        solve(sy, nx + 1, ey, ex);
+      } else {
+        solve(sy, sx, ny + 1, ex);
+        solve(ny + 1, sx, ey, ex);
+      }
+      return;
+    }
+  };
+
+  solve(0, 0, h, w);
   for (int i = 0; i < h; ++i) {
     for (int j = 0; j < w; ++j) {
       std::cout << ans[i][j] << " \n"[j == w - 1];
