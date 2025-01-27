faa24d4952de382d7d8d84cb95f0f451e54b9ca3
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-01-27 02:52:30 +0000

    [abc085] abc085_d

--- abc085/abc085_d/abc085_d.cpp
+++ abc085/abc085_d/abc085_d.cpp
@@ -5,25 +5,24 @@ int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
   int n, h;
   std::cin >> n >> h;
-  std::vector<std::tuple<int, int, int>> p;
+  std::priority_queue<std::pair<int, bool>> pq;
   for (int i = 0; i < n; ++i) {
     int a, b;
     std::cin >> a >> b;
-    p.push_back({a, 0, i});
-    p.push_back({b, 1, i});
+    pq.push({a, 0});
+    pq.push({b, 1});
   }
 
-  sort(p.begin(), p.end());
   int ans = 0;
-  for (int i = 2 * n - 1; i >= 0 && h > 0; --i) {
-    auto [d, t, idx] = p[i];
-    if (t == 1) {
-      h -= d;
-      ans++;
-    } else {
+  while (!pq.empty() && h > 0) {
+    auto [d, t] = pq.top();
+    pq.pop();
+    if (t == 0) {
       ans += (h + d - 1) / d;
       h = 0;
-      break;
+    } else {
+      ans++;
+      h -= d;
     }
   }
 
