dbb92ae5fa2bb58137f444575b912aca23d6100d
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-02-01 11:28:06 +0000

    [abc160] abc160_d

--- abc160/abc160_d/abc160_d.cpp
+++ abc160/abc160_d/abc160_d.cpp
@@ -6,13 +6,15 @@ int main() {
   int n, x, y;
   std::cin >> n >> x >> y;
   std::vector<int> ans(n);
-
   for (int i = 1; i <= n; ++i) {
     for (int j = i + 1; j <= n; ++j) {
-      int d = std::min({abs(i - j), abs(i - x) + abs(j - y) + 1, abs(j - x) + abs(i - y) + 1});
+      int d = abs(j - i);
+      d = std::min(d, std::min(abs(x - i) + abs(y - j) + 1, abs(y - i) + abs(x - j) + 1));
       ans[d]++;
     }
   }
 
-  for (int i = 1; i < n; ++i) std::cout << ans[i] << "\n";
+  for (int i = 1; i < n; ++i) {
+    std::cout << ans[i] << "\n";
+  }
 }
