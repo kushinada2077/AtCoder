842d6d68a5ecff543bfc67c59ed3b85caf9205c5
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-02-14 14:16:08 +0000

    [diverta2019] diverta2019_d

---
+++ diverta2019/diverta2019_d/diverta2019_d.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+
+int main() {
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  i64 N, ans = 0;
+  std::cin >> N;
+  for (i64 p = 1; p * p <= N; ++p) {
+    if (N % p == 0) {
+      i64 a = N / p - 1;
+      if (a > 0 && a > p) {
+        ans += a;
+      }
+    }
+  }
+
+  std::cout << ans << "\n";
+}
