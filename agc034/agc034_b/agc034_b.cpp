a70c5d2aa3d82d790cf9ba026fc307b590cf5f47
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-04-03 01:03:38 +0000

    [agc034] agc034_b

--- agc034/agc034_b/agc034_b.cpp
+++ agc034/agc034_b/agc034_b.cpp
@@ -3,16 +3,26 @@ using i64 = long long;
 
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
-  std::string s;
+  std::string s, ns = "";
   std::cin >> s;
   int n = s.size();
-  i64 cnt = 0, ans = 0;
   for (int i = 0; i < n - 1; ++i) {
-    if (s[i] == 'A') {
-      cnt++;
-    } else if (s[i] == 'B' && s[i + 1] == 'C') {
-      ans += cnt;
+    if (s[i] == 'B' && s[i + 1] == 'C') {
+      ns += 'D';
       i++;
+    } else {
+      ns += s[i];
+    }
+  }
+
+  i64 ans = 0;
+  n = ns.size();
+  int cnt = 0;
+  for (int i = 0; i < n; ++i) {
+    if (ns[i] == 'A') {
+      cnt++;
+    } else if (ns[i] == 'D') {
+      ans += cnt;
     } else {
       cnt = 0;
     }
