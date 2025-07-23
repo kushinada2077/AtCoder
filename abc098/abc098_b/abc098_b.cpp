d85570d961dbd2f2993418621d91f70147874672
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-07-23 06:58:18 +0000

    [abc098] abc098_b

--- abc098/abc098_b/abc098_b.cpp
+++ abc098/abc098_b/abc098_b.cpp
@@ -8,14 +8,18 @@ int main() {
   std::cin >> N >> S;
   int ans = 0;
   for (int i = 1; i < N; ++i) {
-    std::string s1 = S.substr(0, i);
-    std::string s2 = S.substr(i);
-    std::set<char> A1(s1.begin(), s1.end()), A2(s2.begin(), s2.end());
-    int cnt = 0;
-    for (auto c : A1) {
-      cnt += A2.contains(c);
+    std::vector<bool> cnt(26, false);
+    for (int j = 0; j < i; ++j) {
+      cnt[S[j] - 'a'] = true;
     }
-    ans = std::max(ans, cnt);
+    int c = 0;
+    for (int j = i; j < N; ++j) {
+      if (cnt[S[j] - 'a'] == true) {
+        c++;
+        cnt[S[j] - 'a'] = false;
+      }
+    }
+    ans = std::max(ans, c);
   }
   std::cout << ans << "\n";
 }
