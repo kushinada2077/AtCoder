af96e628c519eb0be84ce2e7d02962507e78c2ea
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-07-23 08:41:39 +0000

    [abc107] abc107_b

--- abc107/abc107_b/abc107_b.cpp
+++ abc107/abc107_b/abc107_b.cpp
@@ -9,67 +9,31 @@ int main() {
   for (int i = 0; i < H; ++i) {
     std::cin >> a[i];
   }
-  std::vector<bool> disabled_col(H), disabled_row(W);
-
-  auto check_row = [&](int y) {
-    bool ret = true;
-    if (!disabled_row[y]) {
-      for (int x = 0; x < W; ++x) {
-        if (disabled_col[x]) {
-          continue;
-        }
-        ret &= a[y][x] == '.';
-      }
-    } else {
-      ret = false;
+  std::vector<bool> row(H), col(W);
+  for (int i = 0; i < H; ++i) {
+    bool ok = true;
+    for (int j = 0; j < W; ++j) {
+      ok &= a[i][j] == '.';
     }
-    return ret;
-  };
-
-  auto check_col = [&](int x) {
-    bool ret = true;
-    if (!disabled_col[x]) {
-      for (int y = 0; y < H; ++y) {
-        if (disabled_row[y]) {
-          continue;
-        }
-        ret &= a[y][x] == '.';
-      }
-    } else {
-      ret = false;
+    if (ok) {
+      row[i] = true;
     }
-    return ret;
-  };
+  }
 
-  while (true) {
-    int tx = -1, ty = -1;
+  for (int j = 0; j < W; ++j) {
+    bool ok = true;
     for (int i = 0; i < H; ++i) {
-      if (check_row(i)) {
-        disabled_row[i] = true;
-        ty = i;
-      }
+      ok &= a[i][j] == '.';
     }
-
-    for (int i = 0; i < W; ++i) {
-      if (check_col(i)) {
-        disabled_col[i] = true;
-        tx = i;
-      }
-    }
-
-    if (ty == -1 && tx == -1) {
-      break;
+    if (ok) {
+      col[j] = true;
     }
   }
 
   for (int i = 0; i < H; ++i) {
-    if (disabled_row[i]) {
-      continue;
-    }
+    if (row[i]) continue;
     for (int j = 0; j < W; ++j) {
-      if (disabled_col[j]) {
-        continue;
-      }
+      if (col[j]) continue;
       std::cout << a[i][j];
     }
     std::cout << "\n";
