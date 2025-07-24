516eb75d308960c0a03e2ec2f2879cd159a64e4b
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-07-24 04:25:47 +0000

    [abc073] abc073_c

--- abc073/abc073_c/abc073_c.cpp
+++ abc073/abc073_c/abc073_c.cpp
@@ -5,24 +5,19 @@ int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
   int N;
   std::cin >> N;
-  std::vector<int> A(N), B(N);
+  std::vector<int> A(N);
   for (int i = 0; i < N; ++i) {
     std::cin >> A[i];
-    B[i] = A[i];
   }
-  sort(B.begin(), B.end());
-  B.erase(std::unique(B.begin(), B.end()), B.end());
-  for (int i = 0; i < N; ++i) {
-    A[i] = std::lower_bound(B.begin(), B.end(), A[i]) - B.begin();
-  }
-
-  std::vector<int> c(N);
-  for (int i = 0; i < N; ++i) {
-    c[A[i]] ^= 1;
-  }
+  sort(A.begin(), A.end());
   int ans = 0;
-  for (int i = 0; i < N; ++i) {
-    ans += c[i] == 1;
+  for (int i = 0; i < N;) {
+    int cc = A[i], f = 0;
+    while (cc == A[i]) {
+      i++;
+      f++;
+    }
+    ans += f % 2;
   }
   std::cout << ans << "\n";
 }
