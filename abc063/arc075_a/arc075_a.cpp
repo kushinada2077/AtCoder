4e949af124dbe7882a1cf3c641dbc007d22d6ab5
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-07-24 03:23:53 +0000

    [abc063] arc075_a

--- abc063/arc075_a/arc075_a.cpp
+++ abc063/arc075_a/arc075_a.cpp
@@ -3,27 +3,31 @@ using i64 = long long;
 
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
-  int N;
+  int N, sum = 0;
   std::cin >> N;
-  std::vector<int> ten;
-  int sum = 0;
+  std::vector<int> s(N);
   for (int i = 0; i < N; ++i) {
-    int s;
-    std::cin >> s;
-    if (s % 10 != 0) {
-      ten.push_back(s);
-    }
-    sum += s;
+    std::cin >> s[i];
+    sum += s[i];
   }
-  sort(ten.begin(), ten.end());
 
-  if (sum % 10 == 0) {
-    if (ten.size() > 0) {
-      sum -= ten.front();
-    } else {
-      sum = 0;
+  std::vector dp(N + 1, std::vector<int>(sum + 1, 0));
+  dp[0][0] = 1;
+  for (int i = 1; i <= N; ++i) {
+    for (int j = 0; j <= sum; ++j) {
+      dp[i][j] = dp[i - 1][j];
+      if (j - s[i - 1] >= 0) {
+        dp[i][j] = std::max(dp[i][j], dp[i - 1][j - s[i - 1]]);
+      }
     }
   }
 
-  std::cout << sum << "\n";
+  int ans = 0;
+  for (int i = 0; i <= sum; ++i) {
+    if (dp[N][i] == 1 && i % 10 != 0) {
+      ans = std::max(ans, i);
+    }
+  }
+
+  std::cout << ans << "\n";
 }
