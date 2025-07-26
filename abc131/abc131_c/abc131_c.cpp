7dc518de365f9198a7610f7de9652b7b730eb880
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-07-26 05:34:47 +0000

    [abc131] abc131_c

--- abc131/abc131_c/abc131_c.cpp
+++ abc131/abc131_c/abc131_c.cpp
@@ -5,13 +5,8 @@ int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
   i64 A, B, C, D;
   std::cin >> A >> B >> C >> D;
-  i64 ans = B - A + 1, E = std::lcm(C, D);
-
-  ans -= B / C;
-  ans += (A - 1) / C;
-  ans -= B / D;
-  ans += (A - 1) / D;
-  ans += B / E;
-  ans -= (A - 1) / E;
+  i64 E = std::lcm(C, D);
+  i64 restB = B / C + B / D - B / E, restA = (A - 1) / C + (A - 1) / D - (A - 1) / E;
+  i64 ans = B - A + 1 - (restB - restA);
   std::cout << ans << "\n";
 }
