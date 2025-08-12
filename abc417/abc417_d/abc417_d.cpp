b670af30565d6e4553b6969749ea7780ea309202
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-08-12 10:56:03 +0000

    [abc417] abc417_d

--- abc417/abc417_d/abc417_d.cpp
+++ abc417/abc417_d/abc417_d.cpp
@@ -1,51 +1,40 @@
 #include <bits/stdc++.h>
 using i64 = long long;
 
-const int MAX = 30000;
+const int MAX = 1001;
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
   int N;
   std::cin >> N;
-  std::vector<int> P(N), A(N), B(N), sum(N);
+  std::vector<int> P(N), A(N), B(N), pref(N + 1);
   for (int i = 0; i < N; ++i) {
     std::cin >> P[i] >> A[i] >> B[i];
-    if (i > 0) sum[i] += sum[i - 1];
-    sum[i] += B[i];
+    pref[i + 1] = pref[i] + B[i];
   }
 
-  std::vector dp(N + 1, std::vector<int>(1001, 0));
-  for (int i = 0; i < 1001; ++i) {
-    if (i > P[N - 1]) {
-      dp[N - 1][i] = std::max(0, i - B[N - 1]);
-    } else {
-      dp[N - 1][i] = i + A[N - 1];
-    }
+  std::vector dp(N, std::vector<int>(MAX, 0));
+  for (int i = 0; i < MAX; ++i) {
+    dp[N - 1][i] = P[N - 1] >= i ? i + A[N - 1] : std::max(0, i - B[N - 1]);
   }
 
   for (int i = N - 2; i >= 0; --i) {
-    for (int j = 0; j < 1001; ++j) {
-      if (j > P[i]) {
-        dp[i][j] = dp[i + 1][std::max(0, j - B[i])];
-      } else {
-        dp[i][j] = dp[i + 1][j + A[i]];
-      }
+    for (int j = 0; j < MAX; ++j) {
+      if (P[i] >= j) dp[i][j] = dp[i + 1][j + A[i]];
+      else dp[i][j] = dp[i + 1][std::max(0, j - B[i])];
     }
   }
 
   int Q;
   std::cin >> Q;
   for (int i = 0; i < Q; ++i) {
-    int X;
-    std::cin >> X;
-    if (X > 1000) {
-      auto j = std::lower_bound(sum.begin(), sum.end(), X - 1000) - sum.begin();
-      if (j + 1 < N) {
-        std::cout << dp[j + 1][std::max(0, X - sum[j])] << "\n";
-      } else {
-        std::cout << std::max(0, X - sum[N - 1]) << "\n";
-      }
-    } else {
-      std::cout << dp[0][X] << "\n";
+    int x;
+    std::cin >> x;
+    auto p = std::lower_bound(pref.begin(), pref.end(), std::max(0, x - MAX + 1)) - pref.begin();
+    if (p >= N) {
+      std::cout << std::max(0, x - pref[N]) << "\n";
+      continue;
     }
+    x -= pref[p];
+    std::cout << dp[p][x] << "\n";
   }
 }
