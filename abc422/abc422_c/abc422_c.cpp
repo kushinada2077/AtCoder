561b30af289b026400c55707686eec1a5c2b80d3
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-09-20 11:56:18 +0000

    [abc422] abc422_c

--- abc422/abc422_c/abc422_c.cpp
+++ abc422/abc422_c/abc422_c.cpp
@@ -4,8 +4,7 @@ using i64 = long long;
 void solve() {
   i64 a, b, c;
   std::cin >> a >> b >> c;
-  if (a > c) std::swap(a, c);
-  std::cout << std::min(a, (a + b + c) / 3) << "\n";
+  std::cout << std::min({a, c, (a + b + c) / 3}) << "\n";
 }
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
