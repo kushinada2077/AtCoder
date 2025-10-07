a5786dd1c4ba622c6dcd16bdac88e2f30e0a46ec
kushinada <111179843+kushinada2077@users.noreply.github.com>
2025-10-07 03:19:54 +0000

    [abc426] abc426_c

--- abc426/abc426_c/abc426_c.cpp
+++ abc426/abc426_c/abc426_c.cpp
@@ -2,33 +2,28 @@
 using i64 = long long;
 using P = std::pair<int, int>;
 
-int findp(std::vector<int>& parent, int x) {
-  if (parent[x] == x) return x;
-  return parent[x] = findp(parent, parent[x]);
-}
 int main() {
   std::cin.tie(nullptr)->sync_with_stdio(false);
   int N, Q;
   std::cin >> N >> Q;
-
-  std::vector<int> parent(N + 1);
-  std::vector<int> cnt(N + 1, 0);
-
-  for (int i = 0; i <= N; ++i) parent[i] = i;
-  for (int i = 1; i <= N; ++i) cnt[i] = 1;
-
-  while (Q--) {
-    int X, Y;
-    std::cin >> X >> Y;
-    int upgraded = 0;
-    int v = findp(parent, X);
-    while (v > 0 && v <= X) {
-      upgraded += cnt[v];
-      cnt[Y] += cnt[v];
-      cnt[v] = 0;
-      parent[v] = findp(parent, v - 1);
-      v = parent[v];
+  std::vector<int> cnt(N + 1, 1);
+  cnt[0] = 0;
+  int cur = 0;
+  for (int i = 0; i < Q; ++i) {
+    int x, y;
+    std::cin >> x >> y;
+    if (cur > x) {
+      std::cout << "0\n";
+      continue;
     }
-    std::cout << upgraded << '\n';
+    int o = 0;
+    for (int j = cur; j <= x; ++j) {
+      o += cnt[j];
+    }
+
+    cur = x + 1;
+    cnt[y] += o;
+
+    std::cout << o << "\n";
   }
 }
